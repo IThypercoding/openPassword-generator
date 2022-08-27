52315c12187d2466e3bb30e76a8bd7b8b0fcaf39
IThypercoding <librigiulio@gmail.com>
2022-08-27 12:55:55 +0200

    Less bugs

--- getpassgui.py
+++ getpassgui.py
@@ -18,28 +18,41 @@ layout1 = [  [sg.Text('Password lenght...')],
          orientation='horizontal',
          font=('Helvetica', 12), key="-LE-")],
             [sg.Text('This password can have:')],
-            [sg.Checkbox('numbers', key="-NU-")],
-            [sg.Checkbox('symbols', key="-SY-")],
+            [sg.Checkbox('numbers', key="-NU-", default=True)],
+            [sg.Checkbox('symbols', key="-SY-", default=True)],
             [sg.Button('Ok')]
             ]
 #layout2 = [[sg.Text('Your password is')],[sg.Text(password)]]
 
 window1 = sg.Window('Password maker 1.0', layout1)
-event, values = window1.read()
-lenght_pass_raw = values["-LE-"]
-lenght_pass = int(lenght_pass_raw)
-Full_string = lower_case + upper_case
-if values["-NU-"] == True:
-    Full_string = Full_string + numbers
-if values["-SY-"] == True:
-    Full_string = Full_string + symbols
-lenght_pass = int(lenght_pass_raw)
-password = "".join(random.sample(Full_string, lenght_pass))
-layout2 = [[sg.Text('Your password is')],[sg.Text(password)]]
-window1 = sg.Window('Output', layout2, margins=(50, 50))
+#----------------------------------------------------------------
+#event, values = window1.read()
+#lenght_pass_raw = values["-LE-"]
+#lenght_pass = int(lenght_pass_raw)
+#Full_string = lower_case + upper_case
+#if values["-NU-"] == True:
+#    Full_string = Full_string + numbers
+#if values["-SY-"] == True:
+#    Full_string = Full_string + symbols
+#lenght_pass = int(lenght_pass_raw)
+#password = "".join(random.sample(Full_string, lenght_pass))
+#layout2 = [[sg.Text('Your password is')],[sg.Text(password)]]
+#window1 = sg.Window('Output', layout2, margins=(50, 50))
+#----------------------------------------------------------------
 while True:
     event, values = window1.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window1 or clicks cancel
         break
-
+    elif event == 'Ok':
+        lenght_pass_raw = values["-LE-"]
+        lenght_pass = int(lenght_pass_raw)
+        Full_string = lower_case + upper_case
+        if values["-NU-"] == True:
+            Full_string = Full_string + numbers
+        if values["-SY-"] == True:
+            Full_string = Full_string + symbols
+        lenght_pass = int(lenght_pass_raw)
+        password = "".join(random.sample(Full_string, lenght_pass))
+        layout2 = [[sg.Text('Your password is')],[sg.Text(password)]]
+        window1 = sg.Window('Output', layout2, margins=(50, 50))
 window1.close()
